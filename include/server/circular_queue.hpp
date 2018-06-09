0fd1df91fae6c2b6605a299f1d4e0c781add6f1d
jwqe764241 <jwqe764241@gmail.com>
2018-06-09 23:17:22 +0900

    add circular_queue to contain event

--- include/server/circular_queue.hpp
+++ include/server/circular_queue.hpp
@@ -1,7 +1,71 @@
 #pragma once
 
-template <typename _T>
-class queue
-{
+#include <iostream>
+#include <array>
 
+template <typename t_type, int t_size>
+class circular_queue
+{
+private:
+	/*
+		use modulo operation to check bound
+		so, add one more space in container
+	*/
+	std::array<t_type, t_size + 1> container;
+
+public:
+	int front;
+	int rear;
+
+	circular_queue()
+		: front(0), rear(0), container({0})
+	{
+	}
+
+	~circular_queue()
+	{
+	}
+
+	void enqueue(t_type element)
+	{
+		if(!full())
+		{
+			container[rear] = element;
+			rear = (rear + 1) % container.size();
+		}
+	}
+
+	t_type dequeue()
+	{
+		if(!empty())
+		{
+			int temp_pos = front;
+			front = (front + 1) % container.size();
+
+			return container[temp_pos];
+		}
+	}
+
+	bool empty()
+	{
+		return front == rear;
+	}
+
+	bool full()
+	{
+		return ((rear + 1) % container.size()) == front;
+	}
+
+	size_t size()
+	{
+		/*
+			return size with -1 because container size added one more
+		*/
+		return container.size() -1;
+	}
+
+	std::array<t_type, t_size + 1> getArray()
+	{
+		return container;
+	}
 };
