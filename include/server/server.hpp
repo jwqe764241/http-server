c3c8cd98b1793d9db590d20f18c8e2a232b799ef
jwqe764241 <jwqe764241@gmail.com>
2018-07-03 23:19:33 +0900

    add server class need socket handler in accpet, need to test server class

--- include/server/server.hpp
+++ include/server/server.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <asio.hpp>
+#include <csignal>
+
+#include <iostream>
+
 #include "utils/utils.hpp"
 #include "asio.hpp"
 #include "utils/basic_option.hpp"
@@ -13,13 +18,32 @@ class server
 private:
 	server_option option;
 
+	asio::io_service io_service;
+	asio::signal_set signal;
+	asio::ip::tcp::socket listen_socket;
+	asio::ip::tcp::acceptor acceptor;
+
+
+private:
+	//accept connection and reday to communication
+	void run();
+
+	void onAccept(const asio::error_code error_code);
+	void onStop(const asio::error_code error_code);
 public:
+	//initialize server with default option
 	server();
-	server(server_option option);
+
 	//Not allow assign, copy, move
 	server(const server& server) = delete;
 	server(server&& server) = delete;
 	server& operator=(const server& server) = delete;
+
+	//server start end function
+	//get all resource
+	void start(server_option opyion);
+	//release all resource
+	void exit();
 
 
 
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,8 +2,62 @@
 
 _IMPLEMENT_SCOPE
 
-server::server(server_option option)
+server::server(): 
+	acceptor(io_service),
+	listen_socket(io_service),
+	signal(io_service)
 {
+	//
+
+}
+
+void server::run()
+{
+	io_service.run();
+}
+
+
+void server::onAccept(const asio::error_code error_code)
+{
+	if(!acceptor.is_open())
+	{
+		return;
+	}
+	
+	if(!error_code)
+	{
+		std::cout << "Yeah" << std::endl;
+
+		auto sock = std::move(listen_socket);
+	}
+
+	acceptor.async_accept(listen_socket, std::bind(&server::onAccept, this, std::placeholders::_1));
+}
+
+void server::onStop(const asio::error_code error_code)
+{
+	acceptor.close();
+}
+
+
+void server::start(server_option option)
+{
+	asio::ip::tcp::resolver resolver(io_service);
+	asio::ip::tcp::resolver::query query({option["IP"], option["PORT"]});
+
+	asio::ip::tcp::endpoint endpoint = *resolver.resolve(query);
+	acceptor.open(endpoint.protocol());
+	acceptor.bind(endpoint);
+	acceptor.listen(asio::socket_base::max_connections);
+
+	acceptor.async_accept(listen_socket, std::bind(&server::onAccept, this, std::placeholders::_1));
+
+	run();
+}
+
+void server::exit()
+{
+	signal.async_wait(std::bind(&server::onStop, this, std::placeholders::_1));
 }
 
 _IMPLEMENT_END
