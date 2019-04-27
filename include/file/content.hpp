bcc4f79449195f10ccd216b965e45aed99e3bd92
jwqe764241 <jwqe764241@gmail.com>
2019-04-27 23:05:44 +0900

    write content

--- include/file/content.hpp
+++ include/file/content.hpp
@@ -1,17 +1,30 @@
 #pragma once
 
 #include <string>
+#include <map>
 
 #include "file\path.hpp"
+
+static std::map<std::string, std::string> type_map =
+{
+	{"",	 "application/octet-stream"},
+	{"css",  "text/css"},
+	{"html", "text/html"},
+	{"htm",  "text/html"},
+	{"js",   "text/javascript"},
+	{"txt",  "text/plain"},
+	{"xml",  "text/xml"}
+};
 
 class content
 {
 private:
-	std::string path;
-
+	std::string location;
+	path elements;
 public:
 	content(std::string path);
 
-	std::string get_content_type();
+	std::string get_extension();
+	std::string get_type();
 
 };
--- src/file/content.cpp
+++ src/file/content.cpp
@@ -1,6 +1,31 @@
 #include "file/content.hpp"
 
-content::content(std::string path)
-	: path(path)
+content::content(std::string location)
+try : location(location)
 {
+	elements = path(location);
+}
+catch (...)
+{
+	throw;
+}
+
+std::string content::get_extension()
+{
+	std::string last_element = elements.get_last();
+
+	size_t pos = last_element.find_last_of('.');
+	if (pos != std::string::npos)
+	{
+		return last_element.substr(pos);
+	}
+	else
+	{
+		return "";
+	}
+}
+
+std::string content::get_type()
+{
+	return type_map[get_extension()];
 }
