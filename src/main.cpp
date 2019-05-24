182e83af6b67d866d80ed438f4f89ecd892f0e1b
jwqe764241 <jwqe764241@gmail.com>
2019-05-24 22:58:55 +0900

    add option in start

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
 #include "server/server.hpp"
+#include "utils/file_option.hpp"
 
 int main(int argc, char ** argv)
 {
-	server::server server(1, 300);
-	server.start("127.0.0.1", "80");
+
+	file_option option;
+
+	try
+	{
+		option = file_option(std::ifstream("TestOption.opt"));
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	try
+	{
+		server::server server(
+			option.has("worker") ? std::stoi(option["worker"]) : 1,
+			option.has("max_task") ? std::stoi(option["max_task"]) : 200);
+
+		server.start(
+			option.has("ip") ? option["ip"] : "127.0.0.1",
+			option.has("port") ? option["port"] : "8080");
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	system("pause");
 	return 0;
-
 }
