0222797d6b345e95e225b23c9711d49ab363afb1
jwqe764241 <jwqe764241@gmail.com>
2019-07-17 23:08:52 +0900

    add cmd option

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,71 @@
+#include "get_option.hpp"
 #include "server/server.hpp"
 #include "utils/file_option.hpp"
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
-	file_option option;
+	using namespace cmd;
 
-	try
-	{
-		option = file_option(std::ifstream("TestOption.opt"));
-	}
-	catch (const std::ifstream::failure& e)
-	{
-		std::cout << "No option file or cannot access to option file," << "\n";
-		std::cout << "Will use default options instead." << "\n\n";
-	}
-	catch (const std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	parser parser(argc, argv, {
+		option("--option", "-o", 1),
+		option("--ip", "-i", 1),
+		option("--port", "-p", 1),
+		option("--root", "-r", 1),
+		option("--worker", "-w", 1),
+		option("--task", "-t", 1)
+	});
 
-	try
+	if (parser.has_option("--option"))
 	{
-		web::server server(
-			option.has("workers") ? std::stoi(option["workers"]) : 1,
-			option.has("max_task") ? std::stoi(option["max_task"]) : 200);
+		file_option option;
 
-		server.start(
-			option.has("ip") ? option["ip"] : "127.0.0.1",
-			option.has("port") ? option["port"] : "8080",
-			option.has("root_path") ? option["root_path"] : "./");
+		try
+		{
+			option = file_option(std::ifstream(parser.get_arguments("--option")[0]));
+		}
+		catch (const std::ifstream::failure& e)
+		{
+			std::cout << "No option file or cannot access to option file," << "\n";
+			std::cout << "Will use default options instead." << "\n\n";
+		}
+		catch (const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+
+		try
+		{
+			web::server server(
+				option.has("workers") ? std::stoi(option["workers"]) : 1,
+				option.has("max_task") ? std::stoi(option["max_task"]) : 200);
+
+			server.start(
+				option.has("ip") ? option["ip"] : "127.0.0.1",
+				option.has("port") ? option["port"] : "8080",
+				option.has("root_path") ? option["root_path"] : "./");
+		}
+		catch (const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
-	catch (const std::exception& e)
+	else
 	{
-		std::cout << e.what() << std::endl;
+		try
+		{
+			web::server server(
+				parser.has_option("--worker") ? std::stoi(parser.get_arguments("--worker")[0]) : 1,
+				parser.has_option("--task") ? std::stoi(parser.get_arguments("--task")[0]) : 200);
+
+			server.start(
+				parser.has_option("--ip") ? parser.get_arguments("--ip")[0] : "127.0.0.1",
+				parser.has_option("--port") ? parser.get_arguments("--port")[0] : "8080",
+				parser.has_option("--root") ? parser.get_arguments("--root")[0] : "./");
+		}
+		catch (const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
 
 	system("pause");
