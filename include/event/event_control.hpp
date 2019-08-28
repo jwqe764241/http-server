7173e8e1009931a29f52aea29839515675baf4ae
jwqe764241 <jwqe764241@gmail.com>
2019-08-28 16:42:26 +0900

    Change control to event_control
    i think wrong the control's  structure. so delete all members

---
+++ include/event/event_control.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+#include "event/event.hpp"
+
+class event_control
+{
+private:
+
+public:
+};
--- src/event/control.cpp
+++
@@ -1,7 +0,0 @@
-#include "event/control.hpp"
-
-control::control(std::shared_ptr<event> handler, std::string url)
-	: handler(handler), url(url)
-{
-
-}
