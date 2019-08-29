13dd4476f630fc3b0698da6c2d49c697d29ae8f7
jwqe764241 <jwqe764241@gmail.com>
2019-08-29 18:19:25 +0900

    Add definition of event_control

--- include/event/event_control.hpp
+++ include/event/event_control.hpp
@@ -2,12 +2,26 @@
 
 #include <string>
 #include <memory>
+#include <map>
+#include <functional>
+#include <initializer_list>
 
 #include "event/event.hpp"
+
+using handler_function = std::function<std::shared_ptr<event>()>;
+using event_control_pair = std::pair<std::string, handler_function>;
 
 class event_control
 {
 private:
+	std::map<std::string, handler_function> list;
 
 public:
+	event_control();
+	event_control(std::initializer_list<event_control_pair> controls);
+
+	void add_control(std::string url, handler_function handler);
+	void add_control(event_control_pair pair);
+
+	std::shared_ptr<event> get_event_handler(std::string url);
 };
